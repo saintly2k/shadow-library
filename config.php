a047325356294c7fbeeb88bbdc699ff8b2f085c9
Moonstone <sejo.gaming.com@gmail.com>
2021-08-23 21:06:48 +0200

    Add files via upload

--- config.php
+++ config.php
@@ -1,14 +1,19 @@
 <?php
 
+//DB Configs
+$host='localhost'; //Database Host
+$username='root'; //Database User
+$password=''; //Database Password
+$dbname = "s-lib"; //Database Name
 
-$host='localhost';
-$username='root';
-$password='';
-$dbname = "s-lib";
+//Admin Configs
+$admin_username = "root"; //Admin Username for adding books
+$admin_password = "87g2qeGDS87t2gehsa"; //Admin Password for adding books (HIGHLY recommended using STRONG password!)
 
-$tab_title = "Shadow Library | H33T.moe Project";
-$page_title = "S-Lib.H33T.moe";
-$page_title_link = "https://s-lib.h33t.moe";
+//Page Configs
+$tab_title = "Shadow Library | H33T.moe Project"; //Shown on the Browser tab
+$page_title = "S-Lib.H33T.moe"; //Shown on the top of the page
+$page_title_link = "https://s-lib.h33t.moe"; //$page_title Link on click
 $back_to_title = "Return to h33t.moe";
 $back_to_title_link = "https://h33t.moe";
 
